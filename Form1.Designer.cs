d90d597254242aa3a8cd31dee709bda1fbd7dec5
Lagoyu <paul@shaddick.net>
2018-11-11 19:58:38 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace HowMuchGlass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NumericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.NumericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.NumericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.NumericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // NumericUpDown1
+            // 
+            this.NumericUpDown1.DecimalPlaces = 2;
+            this.NumericUpDown1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.NumericUpDown1.Location = new System.Drawing.Point(15, 66);
+            this.NumericUpDown1.Name = "NumericUpDown1";
+            this.NumericUpDown1.Size = new System.Drawing.Size(60, 20);
+            this.NumericUpDown1.TabIndex = 0;
+            this.NumericUpDown1.Value = new decimal(new int[] {
+            135,
+            0,
+            0,
+            65536});
+            this.NumericUpDown1.ValueChanged += new System.EventHandler(this.NumericUpDown1_ValueChanged);
+            // 
+            // NumericUpDown3
+            // 
+            this.NumericUpDown3.DecimalPlaces = 2;
+            this.NumericUpDown3.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.NumericUpDown3.Location = new System.Drawing.Point(15, 98);
+            this.NumericUpDown3.Name = "NumericUpDown3";
+            this.NumericUpDown3.Size = new System.Drawing.Size(60, 20);
+            this.NumericUpDown3.TabIndex = 1;
+            this.NumericUpDown3.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            65536});
+            this.NumericUpDown3.ValueChanged += new System.EventHandler(this.NumericUpDown3_ValueChanged);
+            // 
+            // NumericUpDown2
+            // 
+            this.NumericUpDown2.DecimalPlaces = 2;
+            this.NumericUpDown2.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.NumericUpDown2.Location = new System.Drawing.Point(15, 130);
+            this.NumericUpDown2.Name = "NumericUpDown2";
+            this.NumericUpDown2.Size = new System.Drawing.Size(60, 20);
+            this.NumericUpDown2.TabIndex = 2;
+            this.NumericUpDown2.Value = new decimal(new int[] {
+            59,
+            0,
+            0,
+            65536});
+            this.NumericUpDown2.ValueChanged += new System.EventHandler(this.NumericUpDown2_ValueChanged);
+            // 
+            // NumericUpDown4
+            // 
+            this.NumericUpDown4.DecimalPlaces = 2;
+            this.NumericUpDown4.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.NumericUpDown4.Location = new System.Drawing.Point(15, 162);
+            this.NumericUpDown4.Name = "NumericUpDown4";
+            this.NumericUpDown4.Size = new System.Drawing.Size(60, 20);
+            this.NumericUpDown4.TabIndex = 3;
+            this.NumericUpDown4.Value = new decimal(new int[] {
+            11,
+            0,
+            0,
+            131072});
+            this.NumericUpDown4.ValueChanged += new System.EventHandler(this.NumericUpDown4_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "glass height";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "base height";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(81, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "diameter";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(81, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "glass thickness";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Enter values in cm";
+            // 
+            // Label6
+            // 
+            this.Label6.AutoSize = true;
+            this.Label6.Location = new System.Drawing.Point(12, 210);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(119, 13);
+            this.Label6.TabIndex = 10;
+            this.Label6.Text = "volume of glass needed";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::HowMuchGlass.Properties.Resources.Tumbler;
+            this.pictureBox1.Location = new System.Drawing.Point(166, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(195, 272);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Label7
+            // 
+            this.Label7.AutoSize = true;
+            this.Label7.Location = new System.Drawing.Point(12, 234);
+            this.Label7.Name = "Label7";
+            this.Label7.Size = new System.Drawing.Size(96, 13);
+            this.Label7.TabIndex = 12;
+            this.Label7.Text = "max glass capacity";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 289);
+            this.Controls.Add(this.Label7);
+            this.Controls.Add(this.Label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NumericUpDown4);
+            this.Controls.Add(this.NumericUpDown2);
+            this.Controls.Add(this.NumericUpDown3);
+            this.Controls.Add(this.NumericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "How much glass? ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NumericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown NumericUpDown1;
+        private System.Windows.Forms.NumericUpDown NumericUpDown3;
+        private System.Windows.Forms.NumericUpDown NumericUpDown2;
+        private System.Windows.Forms.NumericUpDown NumericUpDown4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label Label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+namespace HowMuchGlass
+{
+public partial class Form1 : Form
+{
+public Form1()
+{
+InitializeComponent();
+}
+private void Form1_Load(object sender, EventArgs e){   decimal OUT1 = (NumericUpDown2.Value) * (NumericUpDown2.Value) * 3.1416M * NumericUpDown3.Value + (NumericUpDown1.Value - NumericUpDown3.Value) * 6.2832M * NumericUpDown2.Value * NumericUpDown4.Value;
+Label6.Text = OUT1.ToString("F2") + "ml of glass needed.";  
+decimal OUT2 = (NumericUpDown1.Value - NumericUpDown3.Value) * (NumericUpDown2.Value/2) * (NumericUpDown2.Value/2) * 3.1416M; Label7.Text = OUT2.ToString("F2") + "ml liquid capacity.";    
+}
+private void NumericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+            decimal OUT1 = (NumericUpDown2.Value) * (NumericUpDown2.Value) * 3.1416M * NumericUpDown3.Value + (NumericUpDown1.Value - NumericUpDown3.Value) * 6.2832M * NumericUpDown2.Value * NumericUpDown4.Value;
+Label6.Text = OUT1.ToString("F2") + "ml of glass needed.";
+decimal OUT2 = (NumericUpDown1.Value - NumericUpDown3.Value) * (NumericUpDown2.Value/2) * (NumericUpDown2.Value/2) * 3.1416M; Label7.Text = OUT2.ToString("F2") + "ml liquid capacity.";
+}
+        private void NumericUpDown3_ValueChanged(object sender, EventArgs e){
+             decimal OUT1 = (NumericUpDown2.Value) * (NumericUpDown2.Value) * 3.1416M * NumericUpDown3.Value + (NumericUpDown1.Value - NumericUpDown3.Value) * 6.2832M * NumericUpDown2.Value * NumericUpDown4.Value;
+                Label6.Text = OUT1.ToString("F2") + "ml of glass needed.";
+                decimal OUT2 = (NumericUpDown1.Value - NumericUpDown3.Value) * (NumericUpDown2.Value/2) * (NumericUpDown2.Value/2) * 3.1416M; Label7.Text = OUT2.ToString("F2") + "ml liquid capacity.";
+            }
+private void NumericUpDown2_ValueChanged(object sender, EventArgs e)
+{
+decimal OUT1 = (NumericUpDown2.Value) * (NumericUpDown2.Value) * 3.1416M * NumericUpDown3.Value + (NumericUpDown1.Value - NumericUpDown3.Value) * 6.2832M * NumericUpDown2.Value * NumericUpDown4.Value;
+Label6.Text = OUT1.ToString("F2") + "ml of glass needed.";
+decimal OUT2 = (NumericUpDown1.Value - NumericUpDown3.Value) * (NumericUpDown2.Value/2) * (NumericUpDown2.Value/2) * 3.1416M;Label7.Text = OUT2.ToString("F2") + "ml liquid capacity.";
+}
+        private void NumericUpDown4_ValueChanged(object sender, EventArgs e)
+        {
+        decimal OUT1 = (NumericUpDown2.Value) * (NumericUpDown2.Value) * 3.1416M * NumericUpDown3.Value + (NumericUpDown1.Value - NumericUpDown3.Value) * 6.2832M * NumericUpDown2.Value * NumericUpDown4.Value;
+        Label6.Text = OUT1.ToString("F2") + "ml of glass needed.";
+        decimal OUT2 = (NumericUpDown1.Value - NumericUpDown3.Value) * (NumericUpDown2.Value/2) * (NumericUpDown2.Value/2) * 3.1416M;Label7.Text = OUT2.ToString("F2") + "ml liquid capacity.";
+        }
+}}
