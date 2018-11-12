a56a1cd1711d290463733a1155e4ee9861da9895
Paul S <lagoyu@users.noreply.github.com>
2018-11-12 15:11:19 +0000

    whitespace change test on surface

--- Form1.cs
+++ Form1.cs
@@ -15,7 +15,7 @@ decimal OUT2 = (NumericUpDown1.Value - NumericUpDown3.Value) * (NumericUpDown2.V
 private void NumericUpDown1_ValueChanged(object sender, EventArgs e)
         {
 
-            decimal OUT1 = (NumericUpDown2.Value) * (NumericUpDown2.Value) * 3.1416M * NumericUpDown3.Value + (NumericUpDown1.Value - NumericUpDown3.Value) * 6.2832M * NumericUpDown2.Value * NumericUpDown4.Value;
+            decimal OUT1 = (NumericUpDown2.Value) *  (NumericUpDown2.Value) * 3.1416M * NumericUpDown3.Value + (NumericUpDown1.Value - NumericUpDown3.Value) * 6.2832M * NumericUpDown2.Value * NumericUpDown4.Value;
 Label6.Text = OUT1.ToString("F2") + "ml of glass needed.";
 decimal OUT2 = (NumericUpDown1.Value - NumericUpDown3.Value) * (NumericUpDown2.Value/2) * (NumericUpDown2.Value/2) * 3.1416M; Label7.Text = OUT2.ToString("F2") + "ml liquid capacity.";
 }
