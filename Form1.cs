6bb6155b8fe98597886c2dd759e38bd2fba99c24
Paul Shaddick <Paul.Shaddick@sgscol.ac.uk>
2018-12-04 10:06:37 +0000

    Meaningful variable names and lines made shorter

--- Form1.cs
+++ Form1.cs
@@ -15,9 +15,12 @@ namespace HowMuchGlass
 
         private void UpdateVolumeOutputs()
         {
-            decimal OUT1 = (numDiameter.Value) * (numDiameter.Value) * 3.1416M * numBaseHeight.Value + (numGlassHeight.Value - numBaseHeight.Value) * 6.2832M * numDiameter.Value * numGlassThickness.Value;
-            labGlassNeeded.Text = OUT1.ToString("F2") + "ml of glass needed.";
-            decimal OUT2 = (numGlassHeight.Value - numBaseHeight.Value) * (numDiameter.Value / 2) * (numDiameter.Value / 2) * 3.1416M; labGlassCapacity.Text = OUT2.ToString("F2") + "ml liquid capacity.";
+            decimal glassNeeded = (numDiameter.Value) * (numDiameter.Value) * 3.1416M * numBaseHeight.Value 
+                + (numGlassHeight.Value - numBaseHeight.Value) * 6.2832M * numDiameter.Value * numGlassThickness.Value;
+            labGlassNeeded.Text = glassNeeded.ToString("F2") + "ml of glass needed.";
+            decimal liquidCapacity = (numGlassHeight.Value - numBaseHeight.Value) * 
+                (numDiameter.Value / 2) * (numDiameter.Value / 2) * 3.1416M;
+            labGlassCapacity.Text = liquidCapacity.ToString("F2") + "ml liquid capacity.";
         }
 
         private void numGlassHeight_ValueChanged(object sender, EventArgs e)
