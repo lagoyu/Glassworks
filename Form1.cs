1b8611f1c8940ad9be558c54e5e3687039ab8a78
Paul Shaddick <Paul.Shaddick@sgscol.ac.uk>
2018-12-04 10:21:00 +0000

    Add comments pointing out line where error is

--- Form1.cs
+++ Form1.cs
@@ -18,10 +18,12 @@ namespace HowMuchGlass
 
         private void UpdateVolumeOutputs()
         {
+            // There are two errors in this calculation
             decimal glassNeeded = (numDiameter.Value) * (numDiameter.Value) * PI * numBaseHeight.Value 
                 + (numGlassHeight.Value - numBaseHeight.Value) * 2 * PI * numDiameter.Value * numGlassThickness.Value;
             const string TWO_DECIMAL_PLACES = "F2";
             labGlassNeeded.Text = glassNeeded.ToString(TWO_DECIMAL_PLACES) + "ml of glass needed.";
+            // This calculation appears to be correct
             decimal liquidCapacity = (numGlassHeight.Value - numBaseHeight.Value) * 
                 (numDiameter.Value / 2) * (numDiameter.Value / 2) * PI;
             labGlassCapacity.Text = liquidCapacity.ToString(TWO_DECIMAL_PLACES) + "ml liquid capacity.";
