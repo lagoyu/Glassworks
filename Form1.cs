720c6abc9fc5e9b0eecc85981dbbe8d6d28f24c0
Paul Shaddick <Paul.Shaddick@berkeleygreenutc.org.uk>
2024-03-19 13:47:11 +0000

    Commenting

--- Form1.cs
+++ Form1.cs
@@ -11,28 +11,34 @@ namespace HowMuchGlass
         }
         private void Form1_Load(object sender, EventArgs e)
         {
+            // Instantiate the glass which this form is designed to work with
             myGlass = new CylindricalGlass();
-            UpdateGlassFromInputs(myGlass);
 
-        }
-
-        private void UpdateGlassFromInputs(CylindricalGlass glass)
-        {
-            glass.Height = nudGlassHeight.Value;
-            glass.BaseHeight = nudBaseHeight.Value;
-            glass.OutsideDiameter = nudDiameter.Value;
-            glass.Thickness = nudGlassThickness.Value;
-
-        }
-
-        private void nudParametersChanged(object sender, EventArgs e)
-        {
+            // Initial property values come from the defaults set in the UI
             UpdateGlassFromInputs(myGlass);
             UpdateOutputsFromGlass(myGlass);
         }
 
+        private void nudParametersChanged(object sender, EventArgs e)
+        {
+            // Glass properties need to be updated and results displayed
+            UpdateGlassFromInputs(myGlass);
+            UpdateOutputsFromGlass(myGlass);
+        }
+
+        private void UpdateGlassFromInputs(CylindricalGlass glass)
+        {
+            // Called to update the glass properties when the form is initialised
+            // and when inputs are changed by the user
+            glass.Height = nudGlassHeight.Value;
+            glass.BaseHeight = nudBaseHeight.Value;
+            glass.OutsideDiameter = nudDiameter.Value;
+            glass.Thickness = nudGlassThickness.Value;
+        }
+
         private void UpdateOutputsFromGlass(CylindricalGlass glass)
         {
+            // Display calculated properties used from the glass object
             labVolumeNeeded.Text = glass.GlassMaterialVolume.ToString("F2") + "ml of glass needed.";
             labGlassCapacity.Text = glass.MaxDrinkVolume.ToString("F2") + "ml liquid capacity.";
         }
